f47109c8e277aba5e6cdf3e4d1cfce34f75c991f
ulipese <felipe.sousa4030@gmail.com>
2025-10-30 23:30:08 -0300

    feat: implement AccountJpaEntity adapter

---
+++ src/main/java/com/ulipese/hexagonalstudy/HexagonalStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ulipese.hexagonalstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ulipese/hexagonalstudy/adapters/outbound/persistence/AccountJpaEntity.java
@@ -0,0 +1,27 @@
+package com.ulipese.hexagonalstudy.adapters.outbound.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Entity
+public class AccountJpaEntity {
+    @Id
+    private String id;
+
+    private BigDecimal balance;
+
+    public AccountJpaEntity() {
+    }
+
+    public AccountJpaEntity(String id, BigDecimal balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+}
